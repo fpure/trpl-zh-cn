fdacd8b219950ccd358d95af466846249ff4cb68
fpure <2238932729@qq.com>
2019-12-17 22:48:23 +0800

    fix & optimize

--- ferris.js
+++ ferris.js
@@ -27,6 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
   meta.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no')
   meta.setAttribute('name', 'viewport')
   document.getElementsByTagName('head')[0].append(meta)
+
+  // table横向滚动
+  document.querySelectorAll('table').forEach(t => {
+    var d = document.createElement('div')
+    d.style.overflowX = 'auto'
+    d.style.maxWidth = '100%'
+    t.parentNode.insertBefore(d, t)
+    d.appendChild(t)
+  })
 })
 
 function attachFerrises(type) {
