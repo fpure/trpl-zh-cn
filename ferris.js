52d39cde723fc69c3883ec617789a7ac7d075e99
fpure <2238932729@qq.com>
2020-02-01 22:06:30 +0800

    clean code

--- ferris.js
+++ ferris.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
   var meta = document.createElement('meta')
   meta.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no')
   meta.setAttribute('name', 'viewport')
-  document.getElementsByTagName('head')[0].append(meta)
+  document.head.append(meta)
 
   // table横向滚动
   document.querySelectorAll('table').forEach(t => {
